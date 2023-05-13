65a41ecdf6aff0353e9b776fc4fcc4386fdcc331
wingxel <wingxel@gmail.com>
2023-05-13 16:15:41 +0300

    update

--- clear_img_data.py
+++ clear_img_data.py
@@ -4,5 +4,6 @@ from lib import get_args
 import os
 
 
-print(f"Currently working directory is {os.getcwd()} - {get_args([])}")
-print("Hello")
+if __name__ == "__main__":
+	print(f"Currently working directory is {os.getcwd()} - {get_args([])}")
+	print("Hello")
