e53992466ec052d6f60a8ade1c2b3b69a2165d86
wingxel <wingxel@gmail.com>
2023-05-13 14:33:54 +0300

    Bug fix

--- clear_img_data.py
+++ clear_img_data.py
@@ -4,4 +4,4 @@ from lib import get_args
 import os
 
 
-print(f"Currently working directory is {os.getcwd()} {get_args([])}")
+print(f"Currently working directory is {os.getcwd()} - {get_args([])}")
