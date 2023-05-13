0b07e017edf8c1ca019da9bf767cb6525f933021
wingxel <wingxel@gmail.com>
2023-05-13 13:57:57 +0300

    Added lib.py and README.md

--- clear_img_data.py
+++ clear_img_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-
+from lib import get_args
 import os
 
-print(f"Currently working directory is {os.getcwd()}")
+
+print(f"Currently working directory is {os.getcwd()} {get_args([])}")
---
+++ lib.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def get_args(provided_args: list) -> dict:
+    got = {}
+    return got
+
